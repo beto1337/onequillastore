4770da3fa72fda0f4b60ffc54b31a5e3eeee89b8
jesus <jesusleira2011@gmail.com>
2016-12-02 10:50:00 -0500

    1

--- core/models/class.Conexion.php
+++ core/models/class.Conexion.php
@@ -7,7 +7,7 @@ class Conexion extends mysqli {
 
   public function __construct() {
     parent::__construct('localhost','root','beto1337','onequilla');
-    $this->connect_errno ? die('Error en la conexión a la base de datos') : null;
+    $this->connect_errno ? die(echo 'Error en la conexión a la base de datos') : null;
     $this->set_charset("utf8");
 }
 
