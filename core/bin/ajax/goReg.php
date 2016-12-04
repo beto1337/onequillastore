66803a7e4315d311a928e85eedc174d938afdd77
jesus <jesusleira2011@gmail.com>
2016-12-04 03:11:55 -0500

    1

--- core/bin/ajax/goReg.php
+++ core/bin/ajax/goReg.php
@@ -4,14 +4,14 @@ $db= new conexion();
 $pass   =Encrypt($_POST['pass']);
 $user   =$_POST['user'];
 $email  =$_POST['email'];
-$cod1   = generarCodigo(8);
+$cod1   = generarCodigo(6);
 $sql3   =  $db -> query("SELECT user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
 $cod2   = $db-> recorrer($sql3)[0];
   $db-> liberar($sql3);
 $sql    = $db -> query("SELECT user FROM users WHERE user='$user' or email='$email' LIMIT 1;");
   if ($db->rows($sql) == 0) {
       while ($cod1 == $cod2) {
-        $cod1 = generarCodigo(8);
+        $cod1 = generarCodigo(6);
         $sql3 =  $db -> query("SELECT user FROM users WHERE codigo_user='$cod1' LIMIT 1;");
         $cod2 = $db-> recorrer($sql3)[0];
         $db-> liberar($sql3);
